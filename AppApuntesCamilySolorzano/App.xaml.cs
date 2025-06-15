62033bd5a75ccbfb7f422c1814054b7707aa7957
Camilyyyy <camilystefaniasoza@gmail.com>
2025-06-15 10:46:55 -0500

    Agregar archivos de proyecto.

---
+++ AppApuntesCamilySolorzano/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppApuntesCamilySolorzano
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppApuntesCamilySolorzano/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppApuntesCamilySolorzano
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
