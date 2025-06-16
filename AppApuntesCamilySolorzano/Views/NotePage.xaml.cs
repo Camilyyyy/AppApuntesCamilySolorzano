5401cf4954ea30435ae5d3eb8684f8b78bbc4a1d
Camilyyyy <camilystefaniasoza@gmail.com>
2025-06-15 20:45:29 -0500

    App Completed

--- AppApuntesCamilySolorzano/Views/NotePage.xaml.cs
+++ AppApuntesCamilySolorzano/Views/NotePage.xaml.cs
@@ -1,9 +1,8 @@
 namespace AppApuntesCamilySolorzano.Views;
-
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
-
+    public string ItemId { set { LoadNote(value); } }
     public NotePage()
     {
         InitializeComponent();
@@ -15,31 +14,37 @@ public partial class NotePage : ContentPage
 
     }
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        File.WriteAllText(_fileName, TextEditor.Text);
+        if(BindingContext is Models.Note note) {
+        File.WriteAllText(note.FileName, TextEditor.Text);
+            await Shell.Current.GoToAsync("..");
+        }
     }
 
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
-
-        TextEditor.Text = string.Empty;
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.FileName))
+                File.Delete(note.FileName);
+        }
+        await Shell.Current.GoToAsync("..");
     }
     
     private void LoadNote (string fileName)
     {
-        Models.Note note = new Models.Note();
-        note.FileName = fileName;
+        Models.Note noteModel = new Models.Note();
+        noteModel.FileName = fileName;
 
-        if (File.Exists(_fileName))
+        if (File.Exists(fileName))
         {
-            note.Date=File.GetCreationTime(fileName);
-            note.Text=File.ReadAllText(fileName);
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
         }
 
-        BindingContext = note;
+        BindingContext = noteModel;
     }
 }
