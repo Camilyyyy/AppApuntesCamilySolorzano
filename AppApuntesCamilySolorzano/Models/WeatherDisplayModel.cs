f3a398c0067d300cb8ff8d4533baebc4ce492a32
Camilyyyy <camilystefaniasoza@gmail.com>
2025-06-26 00:36:22 -0500

    Creation of the Model WeatherDisplayModel

---
+++ AppApuntesCamilySolorzano/Models/WeatherDisplayModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppApuntesCamilySolorzano.Models
+{
+    internal class WeatherDisplayModel
+    {
+        public string Time { get; set; }           
+        public string Temperature { get; set; }    
+        public string Humidity { get; set; }       
+        public string Rain { get; set; }          
+
+        public string LocationText { get; set; }
+        public string WeatherIcon { get; set; }
+        public bool IsRaining { get; set; }
+    }
+}
